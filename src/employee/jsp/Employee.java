b6320df811632201c40fa73ad81b2fade937fcb9
shahbazkhan520441 <shahbazkhan520441@gmail.com>
2024-07-30 12:54:35 +0530

    added greeting

---
+++ src/employee/jsp/Employee.java
@@ -0,0 +1,8 @@
+package employee.jsp;
+
+public class Employee {
+	public static void main(String[] args) {
+		System.out.println(" WELCOME TO EMPLOYEE WAGE COMPUTATION");
+	}
+
+}
---
+++ src/module-info.java
@@ -0,0 +1,8 @@
+/**
+ * 
+ */
+/**
+ * 
+ */
+module EmployeeMangagementSystem {
+}
